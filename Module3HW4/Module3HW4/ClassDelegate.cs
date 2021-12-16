e1ae0ee6d43bb38853dce362b62aa3f41e925ca4
AntSenokosov <a.y.senokosov@gmail.com>
2021-12-16 19:33:22 +0200

    Add class ClassDelegate

---
+++ Module3HW4/Module3HW4/ClassDelegate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3HW4
+{
+    public class ClassDelegate
+    {
+        public double Sum(double x, double y) => x + y;
+
+        public void Result(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
